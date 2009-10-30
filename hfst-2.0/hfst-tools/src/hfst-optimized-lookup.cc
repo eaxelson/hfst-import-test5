3037af5e7616ad1bfa3faaa62878a4c1d75738ef
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2009-10-30 07:47:29 +0000

    optimized-lookup was missing a short flag

--- hfst-2.0/hfst-tools/src/hfst-optimized-lookup.cc
+++ hfst-2.0/hfst-tools/src/hfst-optimized-lookup.cc
@@ -94,7 +94,7 @@ int main(int argc, char **argv)
 	};
       
       int option_index = 0;
-      c = getopt_long(argc, argv, "hVvqsewn:", long_options, &option_index);
+      c = getopt_long(argc, argv, "hVvqsewun:", long_options, &option_index);
 
       if (c == -1) // no more options to look at
 	break;
