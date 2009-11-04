1aa6281c619ca2dc8bc474607d6b9c4f7e93e9c5
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2009-11-04 12:04:10 +0000

    Fixed bug in using escaped symbols in set constructions

--- hfst-2.0/hfst-tools/hfst-twolc/src/AlphabetCollector.h
+++ hfst-2.0/hfst-tools/hfst-twolc/src/AlphabetCollector.h
@@ -33,11 +33,11 @@ struct CmpSymbolPairs
   bool operator() (const SymbolPair &p1, 
 		   const SymbolPair &p2)
   {
-    if (strcmp(p1.first,p2.first) == 0)
-      {
-	return strcmp(p1.second,p2.second) < 0;
-      }
-    return strcmp(p1.first,p2.first) < 0;
+    if (lax_str_cmp()(p2.first,p1.first))
+      { return false; }
+    if (lax_str_cmp()(p1.first,p2.first))
+      { return true; }
+    return lax_str_cmp()(p1.second,p2.second);
   }
 };
 
