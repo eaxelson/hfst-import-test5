d00c5cd58dbbae44c9e969c8ee2337a9f807d6fb
eaxelson <eaxelson@users.noreply.github.com>
2010-04-07 13:19:59 +0000

    expanding unknowns/identities and harmonizing should now work in Tropical ofst

--- hfst3_proposition/test.cc
+++ hfst3_proposition/test.cc
@@ -3,21 +3,16 @@
 
 using namespace hfst;
 
-int main(int argc, char **argv) {
-  HfstMutableTransducer t;
-  HfstState second_state = t.add_state();
-  t.set_final_weight(second_state, 0.5);
-  t.add_transition(0, "foo", "bar", 0.3, second_state);
-  HfstState initial = t.get_initial_state();
-  cout << "initial state: " << initial << "\n";
+void print(HfstMutableTransducer &t)
+{
   HfstStateIterator it(t);
   while (not it.done()) {
     HfstState s = it.value();
     HfstTransitionIterator IT(t,s);
     while (not IT.done()) {
-      fprintf(stderr, "loop starts\n");
+      //fprintf(stderr, "loop starts\n");
       HfstTransition tr = IT.value();
-      fprintf(stderr, "transition\n");
+      //fprintf(stderr, "transition\n");
       cout << s << "\t" << tr.get_target_state() << "\t"
 	   << tr.get_input_symbol() << "\t" << tr.get_output_symbol()
 	   << "\t" << tr.get_weight();
@@ -28,33 +23,72 @@ int main(int argc, char **argv) {
       cout << s << "\t" << t.get_final_weight(s) << "\n";
     it.next();
   }
+  return;
+}
+
+int main(int argc, char **argv) {
+  HfstMutableTransducer t;
+  HfstState second_state = t.add_state();
+  HfstState third_state = t.add_state();
+  t.set_final_weight(second_state, 0.5);
+  t.add_transition(0, "foo", "bar", 0.3, second_state);
+  t.add_transition(second_state, "@_UNKNOWN_SYMBOL_@", "@_UNKNOWN_SYMBOL_@", 0.2, third_state);
+  t.add_transition(third_state, "@_IDENTITY_SYMBOL_@", "@_IDENTITY_SYMBOL_@", 0.1, second_state);
+  //HfstState initial = t.get_initial_state();
+  //cout << "initial state: " << initial << "\n";
+
+  print(t);
+  fprintf(stderr, "--\n");
+
+  HfstMutableTransducer t2;
+  HfstState second_state2 = t2.add_state();
+  t2.set_final_weight(second_state2, 0.3);
+  t2.add_transition(0, "@_UNKNOWN_SYMBOL_@", "baz", 1.6, second_state2);
+
+  print(t2);
+
+  HfstTransducer T1(t);
+  HfstTransducer T2(t2);
+
+  fprintf(stderr, "disjunction:\n");
+
+  HfstTransducer DISJ = T1.disjunct(T2, TROPICAL_OFST_TYPE);
+
+  fprintf(stderr, "disjunction done\n");
+
+  HfstMutableTransducer Disj = HfstMutableTransducer(DISJ);
+  fprintf(stderr, "converted to mutable\n");
+  print(Disj);
+
+  return 0;
+
   HfstTransducer T(t);
   ImplementationType type = T.get_type();
-  fprintf(stderr, "%i\n", type);
+  //fprintf(stderr, "%i\n", type);
 
-  fprintf(stderr, "main: (1)\n");
+  //fprintf(stderr, "main: (1)\n");
   T = T.convert(FOMA_TYPE);
-  fprintf(stderr, "main: (1.5)\n");
+  //fprintf(stderr, "main: (1.5)\n");
 
   HfstTransducer Tcopy = HfstTransducer(T);
-  fprintf(stderr, "main: (1.51)\n");
+  //fprintf(stderr, "main: (1.51)\n");
   HfstTransducer disj = T.disjunct(Tcopy, FOMA_TYPE);
 
-  fprintf(stderr, "main: (2)\n");
+  //fprintf(stderr, "main: (2)\n");
   T = T.convert(SFST_TYPE);
-  fprintf(stderr, "main: (3)\n");
+  //fprintf(stderr, "main: (3)\n");
   T = T.convert(TROPICAL_OFST_TYPE);
-  fprintf(stderr, "main: (4)\n");
+  //fprintf(stderr, "main: (4)\n");
   T = T.convert(FOMA_TYPE);
-  fprintf(stderr, "main: (5)\n");
+  //fprintf(stderr, "main: (5)\n");
   type = T.get_type();
   //fprintf(stderr, "%i\n", type);
-  //HfstOutputStream os(LOG_OFST_TYPE);
-  /*os << TR;
-    fprintf(stderr, "\n\n");*/
-  //cout << TR;
+  //HfstOutputStream os(FOMA_TYPE);
+  //os << T;
+  //fprintf(stderr, "\n\n");
+  //cout << T;
   //fprintf(stderr, "\n");
-  fprintf(stderr, "main: (6)\n");
+  //fprintf(stderr, "main: (6)\n");
   // FIX: calling ~HfstTransducer causes a glibc with foma
   return 0;
 }
