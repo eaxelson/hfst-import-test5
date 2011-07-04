384bf9bb8bb5a2de53482536b8532a4e0301ab1f
jonorthwash <jonorthwash@941e2c2b-deac-454f-805a-451daa25f33c>
2011-07-04 08:25:01 +0000

    A bunch of extra features, but uglifying of code..

--- trunk/util-scripts/twolc_debug.py
+++ trunk/util-scripts/twolc_debug.py
@@ -3,9 +3,12 @@
 import os, re, argparse
 from subprocess import Popen, PIPE
 
-parser = argparse.ArgumentParser(prog='twolc_debug', description='Tells which twol rules eliminate which elements of a given form')
+parser = argparse.ArgumentParser(prog='twolc_debug', description='Tells which twol rules exclude which potential outputs of a given form')
 parser.add_argument('-t', '--twol', nargs=1, dest='twolcFile', help='The compiled twol.hfst file')
 parser.add_argument('form', nargs='+', help='A form or forms to parse')
+parser.add_argument('-c', '--correct', dest='correct', help='Correct output of twolc')
+parser.add_argument('-p', '--potential', dest='potential', action='store_true', help='Show all potential outputs produced by rule, as opposed to all outputs excluded by rule')
+parser.add_argument('-w', '--why', dest='why', action='store_true', help='Instead of output forms, tell why the correct form is not being produced')
 
 #twolcFile = "/home/jonathan/quick/apertium/svn/incubator/apertium-tr-ky/.deps/ky.twol.hfst"
 #form = "с ү й > {I} п"
@@ -33,7 +36,8 @@ def get_forms(form, numRules, twolcFile):
 	p2 = Popen(["hfst-strings2fst", "-S"], stdin=p1.stdout, stdout=PIPE)
 	p3 = Popen(["hfst-duplicate", "-n", str(numRules)], stdin=p2.stdout, stdout=PIPE)
 	p4 = Popen(["hfst-compose", twolcFile], stdin=p3.stdout, stdout=PIPE)
-	p5 = Popen(["hfst-fst2strings"], stdin=p4.stdout, stdout=PIPE)
+	# , "-N 10"
+	p5 = Popen(["hfst-fst2strings", "-c 1"], stdin=p4.stdout, stdout=PIPE)
 	p1.stdout.close()
 	p2.stdout.close()
 	p3.stdout.close()
@@ -51,7 +55,7 @@ def get_all_forms(blocks):
 			allForms.add(line)
 	return allForms
 
-def main_loop(twolcFile, form):
+def main_loop(twolcFile, form, correct, potential):
 	rules = {}
 	for (num, rule) in get_rule_names(twolcFile):
 		rules[num] = rule
@@ -63,18 +67,63 @@ def main_loop(twolcFile, form):
 	allForms = get_all_forms(blocks)
 
 	for i in range(1,numRules):
-		eliminatedForms = allForms - set(blocks[i])
+		if potential:
+			eliminatedForms = blocks[i]  # actually, should be "potential forms"
+		else:
+			eliminatedForms = allForms - set(blocks[i])
 		if eliminatedForms != set():
 			print(rules[i])
-			print("\teliminates", eliminatedForms)
+			print("\tcreates", ) if potential else print("\texcludes", )
+			for form in eliminatedForms:
+				#print(form.split(':')[1], correct)
+				if form.split(':')[1] == correct:
+					print('\t\t\033[1;31m'+form+'\033[1;m', )
+				else:
+					print('\t\t'+form, )
+
+def why_loop(twolcFile, form, correct, potential):
+	rules = {}
+	for (num, rule) in get_rule_names(twolcFile):
+		rules[num] = rule
+	numRules = len(rules)
+	blocks = {}
+	for (count, block) in get_forms(form, numRules, twolcFile):
+		blocks[count] = block
+
+	allForms = get_all_forms(blocks)
+
+	found = 0
+	excluded = False
+	correctPre = re.sub(' ','',form)+":"+correct
+	for i in range(1,numRules):
+		potentialForms = blocks[i]
+		excludedForms = allForms - set(blocks[i])
+		if excludedForms != set() or potentialForms != set():
+			if correctPre in excludedForms:
+				print(rules[i]+" excludes form "+form)
+				excluded = True
+			if correctPre in potentialForms:
+				#print(rules[i]+" creates form "+form)
+				found += 1
+				
+		else:
+			print("No forms output by rule "+rules[i]+"!")
+
+	if found==0:
+		print("Correct form ‹ "+correct+" › never created!")
+	elif not excluded:
+		print("This form should be working!")
+
 
 args = parser.parse_args()
 
 if args.form != None and args.twolcFile != None:
 	for form in args.form:
 		print('\n'+form+'\n')
-		main_loop(args.twolcFile[0], form)
+		if not args.why:
+			main_loop(args.twolcFile[0], form, args.correct, args.potential)
+		else:
+			why_loop(args.twolcFile[0], form, args.correct, args.potential)
 #elif args.twolcFile != None:
 else:
-	parser.print_help()
-	
+	parser.print_help()	
