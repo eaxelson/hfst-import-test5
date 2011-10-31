a3115013f05c039db809dd8ba7719637b624654d
mpsilfve <mpsilfve@941e2c2b-deac-454f-805a-451daa25f33c>
2011-10-31 07:02:35 +0000

    An error in the nokia file

--- trunk/cicling-2011-predictive-text-entry/src/t9_suggest_only_hmm.cc
+++ trunk/cicling-2011-predictive-text-entry/src/t9_suggest_only_hmm.cc
@@ -160,12 +160,11 @@ using hfst::HfstTransducerVector;
 
 int main(int argc, char * argv[])
 {
-  if (argc != 7)
+  if (argc != 4)
     {
       std::cerr << "Usage: cat input_1_word_/_line | " << argv[0] 
 		<< " letter_n_gram_lexical_model letter_n_gram_sequence_model"
-		<< " sequencer_model morpheme_concatenation_model "
-		<< "txt_to_nr_fst vowel_harmony_rules "
+		<< " text_to_numbers_fst"
 		<< std::endl;
       exit(1);
     }
@@ -174,50 +173,8 @@ int main(int argc, char * argv[])
 
   HMMTagger letter_n_gram_model(argv[1],argv[2]);
 
-  Sequencer sequencer(argv[3]);
-
-  SequenceModel morpheme_sequence_model(argv[4]);
-
-  HfstInputStream coder_stream(argv[5]);
+  HfstInputStream coder_stream(argv[3]);
   HfstTransducer coder(coder_stream);
-
-  HfstTransducerVector harmony_rule_vector;
-  HfstInputStream harmony_in(argv[6]);
-  while (harmony_in.is_good())
-    { harmony_rule_vector.push_back(HfstTransducer(harmony_in)); }
-  
-  assert(harmony_rule_vector.size() > 1);
-
-  // We should delete these rules, when the program exits, but it doesn't
-  // really make any difference in this case.
-  hfst::implementations::ComposeIntersectRule * first_rule = 
-    new hfst::implementations::ComposeIntersectRule(harmony_rule_vector[0]);
-  hfst::implementations::ComposeIntersectRule * second_rule = 
-    new hfst::implementations::ComposeIntersectRule(harmony_rule_vector[1]);
-
-  hfst::implementations::ComposeIntersectRulePair * rules = 
-    new hfst::implementations::ComposeIntersectRulePair
-    (first_rule,second_rule);
-
-    for (HfstTransducerVector::const_iterator it = 
-	   harmony_rule_vector.begin() + 2;
-         it != harmony_rule_vector.end();
-         ++it)
-    { 
-      rules = new hfst::implementations::ComposeIntersectRulePair
-        (new hfst::implementations::ComposeIntersectRule(*it),rules); 
-    }
-
-
-
-  //clock_t STOP_LOAD = clock();
-
-  //  std::cerr << "Model loaded in " 
-  //	    << (STOP_LOAD - START_LOAD) / (1.0 * CLOCKS_PER_SEC)
-  //	    << std::endl;
-
-  //clock_t START_INPUT = clock();
-  
 
   char line[MAXBUFFER];
   while (std::cin.peek() != EOF)
@@ -230,8 +187,6 @@ int main(int argc, char * argv[])
 	   it != tokenized_line.end();
 	   ++it)	
 	{	  
-	  //clock_t START_HMM = clock();
-
 	  StringVector number_input = pad_and_code_to_numbers(*it,coder);
 	  
 	  HfstBasicTransducer n_gram_weighted_input = 
@@ -240,113 +195,19 @@ int main(int argc, char * argv[])
 	  HfstTransducer n_gram_weighted_fst
 	    (n_gram_weighted_input,TROPICAL_OPENFST_TYPE);
 
-	  /*
-	  n_gram_weighted_fst.transform_weights(multiply_weights);
-
-	  n_gram_weighted_fst.substitute(DEFAULT_SYMBOL,
-					 internal_epsilon,
-					 true,
-					 false);
-
-	  //n_gram_weighted_fst.substitute("||",
-	  //				 internal_epsilon,
-	  //				 true,
-	  //				 true);
+	  HfstTransducer output(n_gram_weighted_fst);
 	  
-	  n_gram_weighted_fst.minimize();
-
-	  n_gram_weighted_fst.substitute
-	    (StringPair("||","<DEFAULT>"),
-	     StringPair("||","||")).minimize();
-	  
-	  //clock_t STOP_HMM = clock();
-
-	  //std::cerr << "Letter n-gram model applied in " 
-	  //	    << (STOP_HMM - START_HMM) / (1.0 * CLOCKS_PER_SEC)
-	  //  << std::endl;
-	  
-	  //clock_t START_SEQUENCER = clock();
-
-	  
-	  HfstTransducer sequenced_input = sequencer[n_gram_weighted_fst];
-
-	  HfstBasicTransducer basic_sequenced_input(sequenced_input);
-
-	  //clock_t STOP_SEQUENCER = clock();
-
-	  //std::cerr << "Sequencer applied in " 
-	  //	    << (STOP_SEQUENCER - START_SEQUENCER) / 
-	  // (1.0 * CLOCKS_PER_SEC)
-	  //    << std::endl;
-	  
-	  //clock_t START_MORPHEME_SEQUENCE = clock();
-
-	  HfstBasicTransducer output_basic_fst = 
-	    morpheme_sequence_model.tag_input(basic_sequenced_input);
-
-	  //clock_t STOP_MORPHEME_SEQUENCE = clock();
-
-	  //	  std::cerr << "Morpheme sequence model applied in " 
-	  //	    << (STOP_MORPHEME_SEQUENCE - START_MORPHEME_SEQUENCE) / 
-	  //(1.0 * CLOCKS_PER_SEC)
-	  // 	    << std::endl;
-
-	  //clock_t START_OUTPUT = clock();
-
-	  HfstTransducer output(output_basic_fst,TROPICAL_OPENFST_TYPE);
-	  
-	  output.substitute(DEFAULT_SYMBOL,
-			    "<L>").minimize();
-	  
-	  //clock_t START_LETTER_RESTORATION = clock();
-
-	  output.substitute(StringPair("<L>",internal_epsilon),
-			    StringPair("<L>","<L>")).minimize();
-
-	  output.substitute(DEFAULT_SYMBOL,
-			    internal_epsilon).
-	    output_project()
-	    .minimize();
-
-	  output = restore_letters(output);
-	  
-	  ComposeIntersectLexicon output_lex(output);
-	  output = 
-	    HfstTransducer(output_lex.compose_with_rules(rules),TROPICAL_OPENFST_TYPE);
-	  
-
 	  output.substitute("||",internal_epsilon);
 	  output.substitute("<L>",internal_epsilon);
 
 	  output
 	    .output_project()
-	    .n_best(5)	     
+	    .n_best(10)	     
 	    .minimize();
-
-	  //std::cerr << output << std::endl;
-
-	  //clock_t STOP_LETTER_RESTORATION = clock();
-
-	  //std::cerr << "Letters restored in " 
-	  //	    << (STOP_LETTER_RESTORATION - START_LETTER_RESTORATION) / 
-	  // (1.0 * CLOCKS_PER_SEC)
-	  //	    << std::endl;
-
-	  */
-
-	  HfstTransducer output(n_gram_weighted_fst);
-	  
-	  output.output_project();
 
 	  HfstTwoLevelPaths output_paths;
 	  
 	  output.extract_paths(output_paths);
-
-	  //clock_t STOP_OUTPUT = clock();
-
-	  //std::cerr << "Output generated in " 
-	  //	    << (STOP_OUTPUT - START_OUTPUT) / (1.0 * CLOCKS_PER_SEC)
-	  //	    << std::endl;
 
 	  std::cout << *it << "\t";
 	  bool found = false;
@@ -364,31 +225,23 @@ int main(int argc, char * argv[])
 		   kt != output_path.second.end();
 		   ++kt)
 		{ 
-		  //std::cout << (kt->first == "<DEFAULT>" ? "" : kt->first); 
 		  output_string += 
 		    (kt->first == "<DEFAULT>" ? "" : kt->first); 
 		}
 
 	      if (output_string == *it)
-		{ 
-		  found = true;
-		  std::cout << pos; 
-		  break;
-		}
+	      	{ 
+	      	  found = true;
+	      	  std::cout << pos; 
+	      	  break;
+	      	}
 	      ++pos;
 	    }
 	  
 	  if (not found)
-	    { std::cout << "infty"; }
+	   { std::cout << "infty"; }
 
 	  std::cout << std::endl;
 	}
     }
-
-  //clock_t STOP_INPUT = clock();
-
-  //std::cerr << "Input processed in " 
-  //	    << (STOP_INPUT - START_INPUT) / (1.0 * CLOCKS_PER_SEC)
-  //	    << std::endl;
-
 }
