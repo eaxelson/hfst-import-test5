23d20e87941b2f4b607c2180384fef4ccdeccb36
eaxelson <eaxelson@users.noreply.github.com>
2011-12-01 15:09:31 +0000

    Made small modifications, now the libraries should be included correctly.

--- debian/debian-test/make-package.sh
+++ debian/debian-test/make-package.sh
@@ -3,57 +3,91 @@
 #  A script for creating the debian package for HFST
 #
 
-hfst_tool_dir=../../hfst3/tools/src/.libs/ # the HFST commandline tools
-hfst_library_dir=../../hfst3/libhfst/src/.libs/ # the HFST library
-hfst_header_dir=../../hfst3/libhfst/src/ # the HFST header files
-impl_library_dir=/usr/local/lib/  # the backend implementation libraries
+#hfst_tool_dir=../../hfst3/tools/src/.libs/ # the HFST commandline tools
+#hfst_library_dir=../../hfst3/libhfst/src/.libs/ # the HFST library
+#hfst_header_dir=../../hfst3/libhfst/src/ # the HFST header files
+#impl_library_dir=/usr/local/lib/  # the backend implementation libraries
 
 
-tools=hfst-invert
+#hfst_tools=hfst-invert # ...
 
 # Copy the tools
-for tool in $tools;
-do
-    cp $hfst_tool_dir/$tool debian/usr/bin/;
-done
+#for tool in $hfst_tools;
+#do
+#    cp $hfst_tool_dir/$tool debian/usr/bin/;
+#done
 strip debian/usr/bin/*
 
-# test..
-# mv debian/usr/bin/hfst-invert debian/usr/bin/hfst-invert-foo
-
 # Copy the libraries
-cp -P $hfst_library_dir/libhfst.so* debian/usr/lib/
-cp -P $impl_library_dir/libfst.so* debian/usr/lib/
+#cp -P $hfst_library_dir/libhfst.so* debian/usr/lib/
+#cp -P $impl_library_dir/libfst.so* debian/usr/lib/
+
 cd debian/usr/lib/
-rm libfst.so
-ln -s -T libfst.so.0.0.0 libfst.so
+
+cp /usr/local/lib/libfst.so.0.0.0 .
+rm -f libfst.so libfst.so.0
+ln -s -T libfst.so.0.0.0 libfst.so.0
+ln -s -T libfst.so.0 libfst.so
+
+cp /usr/local/lib/libsfst.so.0.0.0 .
+rm -f libsfst.so libsfst.so.0
+ln -s -T libsfst.so.0.0.0 libsfst.so.0
+ln -s -T libsfst.so.0 libsfst.so
+
+#cp /usr/local/lib/libsfst.so.1.3 .
+#rm -f libsfst.so.1 libsfst.so
+#ln -s -T libsfst.so.1.3 libsfst.so.1
+#ln -s -T libsfst.so.1 libsfst.so
+
+cp /usr/local/lib/libfoma.so.0.9.14 .
+rm .f libfoma.so.0 libfoma.so
+ln -s -T libfoma.so.0.9.14 libfoma.so.0
+ln -s -T libfoma.so.0 libfoma.so
+
 strip *
 chmod 0644 *
+
+rm libhfst.la
+
 cd ../../..
 
 header_files="HfstTransducer.h hfst_apply_schemas.h HfstDataTypes.h \
-    HfstSymbolDefs.h implementations/SfstTransducer.h \
-    implementations/TropicalWeightTransducer.h \
-    implementations/LogWeightTransducer.h implementations/FomaTransducer.h \
-    implementations/HfstOlTransducer.h HfstTokenizer.h \
+    HfstSymbolDefs.h HarmonizeUnknownAndIdentitySymbols.h HfstExceptionDefs.h \
+    HfstExtractStrings.h HfstFlagDiacritics.h HfstInputStream.h \
+    HfstOutputStream.h HfstTokenizer.h \
     implementations/ConvertTransducerFormat.h \
     implementations/HfstTransitionGraph.h implementations/HfstTransition.h \
     implementations/HfstTropicalTransducerTransitionData.h \
-    implementations/HfstFastTransitionData.h \
+    implementations/HfstFastTransitionData.h"
+
+header_files=$header_files" implementations/SfstTransducer.h"
+
+header_files=$header_files" implementations/FomaTransducer.h"
+
+header_files=$header_files" implementations/TropicalWeightTransducer.h \
+                            implementations/LogWeightTransducer.h"
+
+header_files=$header_files" parsers/LexcCompiler.h"
+
+header_files=$header_files" parsers/XreCompiler.h"
+
+header_files=$header_files" implementations/HfstOlTransducer.h"
+
+header_files=$header_files" \
     implementations/compose_intersect/ComposeIntersectRulePair.h \
     implementations/compose_intersect/ComposeIntersectLexicon.h \
     implementations/compose_intersect/ComposeIntersectRule.h \
-    implementations/compose_intersect/ComposeIntersectFst.h \
-    HarmonizeUnknownAndIdentitySymbols.h HfstExceptionDefs.h \
-    HfstExtractStrings.h HfstFlagDiacritics.h HfstInputStream.h \
-    HfstOutputStream.h parsers/LexcCompiler.h parsers/XreCompiler.h"
+    implementations/compose_intersect/ComposeIntersectFst.h"
+
 
 # Copy the HFST header files
-for header in $header_files;
-do
-    cp -P $hfst_header_dir/$header debian/usr/include/;
-done
+#for header in $header_files;
+#do
+#    cp -P $hfst_header_dir/$header debian/usr/include/;
+#done
 chmod 0644 debian/usr/include/*
+#mkdir debian/usr/include/hfst
+chmod 0755 debian/usr/include/hfst
 
 # Configure the libraries
 cp debian/DEBIAN/control debian/control
@@ -65,6 +99,25 @@ if [ -e debian/usr/share/doc/hfst-dev/changelog.Debian ]; then
     gzip --force --best debian/usr/share/doc/hfst-dev/changelog.Debian;
 fi
 
+# move directory share under usr
+#if [ -d debian/share ]; then
+#    if ! [ -d debian/usr/share ]; then
+#	mkdir debian/usr/share;
+#    fi
+#    cp -R debian/share/* debian/usr/share/;
+#fi
+
+# move directory include under usr
+#if [ -d debian/include ]; then
+#    if ! [ -d debian/usr/share ]; then
+#	mkdir debian/usr/share;
+#    fi
+#    cp -R debian/share/* debian/usr/share/;
+#fi
+
+# gzip the man pages
+gzip --best --force debian/usr/share/man/man1/*.1
+
 # To make a copy without the .svn files
 # tar -c --exclude=.svn debian-test | tar -x -C debian-test-copy
 # cd debian-test-copy
@@ -73,9 +126,8 @@ fi
 
 # Make the package
 fakeroot dpkg-deb --build debian
-#fakeroot dpkg-buildpackage
 
-# Test if the package is ok (lintian will give some warnings, 
+# Test if the package is ok (lintian will probably give some warnings, 
 # see if they should be handled...)
 lintian debian.deb
 
@@ -87,7 +139,3 @@ gunzip debian/usr/share/doc/hfst-dev/changelog.Debian.gz
 
 # and to remove
 # sudo dpkg -r foo-dev
-
-
-
-
