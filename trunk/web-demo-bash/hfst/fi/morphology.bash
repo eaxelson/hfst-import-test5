39ed02980f71943aa2eb741392b519eee4f4ecdf
mie <mie@941e2c2b-deac-454f-805a-451daa25f33c>
2012-12-19 00:15:30 +0000

    Allow multiword processing, split by [:space:]. More prominent missing fail error message

--- trunk/web-demo-bash/hfst/fi/morphology.bash
+++ trunk/web-demo-bash/hfst/fi/morphology.bash
@@ -4,7 +4,7 @@ if test $# -lt 2 ; then
     exit 1
 fi
 
-WORDFORM=$1
+WORDFORM=${1//+/ }
 LL='fi'
 VARIANT=$2
 
@@ -13,24 +13,26 @@ if ! test -r hfst/$LL/morphology.$VARIANT.hfstol ; then
     exit 1
 fi
 
-echo "<p>Analysing word-form <em>"
-echo $WORDFORM | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g'
-echo "</em>:</p>"
+for wf in $WORDFORM ; do
+    echo "<p>Analysing word-form <em>"
+    echo $wf | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g'
+    echo "</em>:</p>"
 
-case $VARIANT in
-    cg)
-        echo "<div>click terms for wordnet search on form / stem</div>"
-        echo "<dl><dt><a href='http://www.ling.helsinki.fi/cgi-bin/fiwn/search?wn=fi&w=$WORDFORM&t=over&sm=Search' title='search fiwn for Finnish $WORDFORM'>"
-        echo $WORDFORM | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g'
-        echo '</a></dt>'
-        echo "$WORDFORM" |\
-            hfst-lookup hfst/$LL/morphology.$VARIANT.hfstol 2>&1 |\
-            awk -F "\t" '{printf("<dd><a href=\"http://www.ling.helsinki.fi/cgi-bin/fiwn/search?wn=fi&w=%s&t=over&sm=Search\" title=\"search fiwn for Finnish %s\">%s</a></dd>\n", gensub(/+.*/, "", "g", $2), gensub(/+.*/, "", "g", $2), $2);}';
-        echo '</dl>';;
-    *)
-        echo "<pre>";
-        echo "$WORDFORM" |\
-            hfst-lookup hfst/$LL/morphology.$VARIANT.hfstol 2>&1 |\
-            sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g';
-        echo "</pre>";;
-esac
+    case $VARIANT in
+        cg)
+            echo "<div>click terms for wordnet search on form / stem</div>"
+            echo "<dl><dt><a href='http://www.ling.helsinki.fi/cgi-bin/fiwn/search?wn=fi&w=$wf&t=over&sm=Search' title='search fiwn for Finnish $wf'>"
+            echo $wf | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g'
+            echo '</a></dt>'
+            echo "$wf" |\
+                hfst-lookup hfst/$LL/morphology.$VARIANT.hfstol 2>&1 |\
+                awk -F "\t" '{printf("<dd><a href=\"http://www.ling.helsinki.fi/cgi-bin/fiwn/search?wn=fi&w=%s&t=over&sm=Search\" title=\"search fiwn for Finnish %s\">%s</a></dd>\n", gensub(/+.*/, "", "g", $2), gensub(/+.*/, "", "g", $2), $2);}';
+            echo '</dl>';;
+        *)
+            echo "<pre>";
+            echo "$wf" |\
+                hfst-lookup hfst/$LL/morphology.$VARIANT.hfstol 2>&1 |\
+                sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g';
+            echo "</pre>";;
+    esac
+done
