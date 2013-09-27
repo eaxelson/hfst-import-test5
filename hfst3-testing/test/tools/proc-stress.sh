360a1774e7dec2fb14357f00e21bdf6eda5918f2
hardwick <hardwick@941e2c2b-deac-454f-805a-451daa25f33c>
2013-09-27 12:08:15 +0000

    Get rid of old devbranches

--- hfst3-testing/test/tools/proc-stress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if test -f cat2dogol ; then
-    for c in 1 256 65536 16777216 ; do
-        if ! yes cat | head -n $c | ../../tools/src/hfst-proc-apertium-proc cat2dogol > /dev/null ; then
-            exit 1
-        fi
-    done
-fi
--- hfst3-testing/tools/src/inc/check-params-common.h
+++
@@ -1,8 +0,0 @@
-
-    if (!outputNamed)
-    {
-        outfilename = hfst_strdup("<stdout>");
-        outfile = stdout;
-        message_out = stderr;
-    }
-
