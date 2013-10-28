788a0452ecc3e2ba4201a6b8f413946e5fe4f48f
eaxelson <eaxelson@941e2c2b-deac-454f-805a-451daa25f33c>
2013-10-28 14:14:49 +0000

    Now releasing debian package of swig bindings should work.

--- trunk/debian/debian-swig/make-debian-package.sh
+++ trunk/debian/debian-swig/make-debian-package.sh
@@ -75,18 +75,25 @@ if (grep "i386" debian/DEBIAN/control > /dev/null 2> /dev/null); then
     ARCHITECTURE=i386
 fi
 
+
 if [ "$PYTHON_VERSION" = "2" ]; then
     sed -i "s/HFSTPY-DEV/hfstpy2-dev/" debian/DEBIAN/control
     sed -i "s/HFSTPY/hfstpy2/" debian/DEBIAN/control
     sed -i "s/PYTHON_MIN_VERSION/2.6/" debian/DEBIAN/control
+    sed -i "s/PYTHON_NAME/python/" debian/DEBIAN/control
+    sed -i "s/HFSTPY/hfstpy2/" debian/usr/share/doc/HFSTPY-DEV/changelog.Debian
     mv debian/usr/share/doc/HFSTPY-DEV debian/usr/share/doc/hfstpy2-dev
-    sed -i "s/HFSTPY/hfstpy2/" debian/usr/share/doc/hfstpy2-dev/changelog.Debian
+    chmod 0755 debian/usr/share/doc/hfstpy2-dev
+    chmod 0644 debian/usr/share/doc/hfstpy2-dev/changelog.Debian
 else
     sed -i "s/HFSTPY-DEV/hfstpy3-dev/" debian/DEBIAN/control
     sed -i "s/HFSTPY/hfstpy3/" debian/DEBIAN/control
     sed -i "s/PYTHON_MIN_VERSION/3.2/" debian/DEBIAN/control
+    sed -i "s/PYTHON_NAME/python3/" debian/DEBIAN/control
+    sed -i "s/HFSTPY/hfstpy3/" debian/usr/share/doc/HFSTPY-DEV/changelog.Debian
     mv debian/usr/share/doc/HFSTPY-DEV debian/usr/share/doc/hfstpy3-dev
-    sed -i "s/HFSTPY/hfstpy3/" debian/usr/share/doc/hfstpy3-dev/changelog.Debian
+    chmod 0755 debian/usr/share/doc/hfstpy3-dev
+    chmod 0644 debian/usr/share/doc/hfstpy3-dev/changelog.Debian
 fi
 
 
@@ -116,7 +123,7 @@ else
     chmod 755 dist-packages &&
     cd dist-packages &&
     cp $HFST_SWIG_DIR/python3-libhfst.py libhfst.py &&
-    echo '#!/usr/bin/python' >> tmp &&
+    echo '#!/usr/bin/python3' >> tmp &&
     cat libhfst.py >> tmp &&
     mv tmp libhfst.py &&
     chmod 755 libhfst.py &&
@@ -128,9 +135,13 @@ fi
 
 cd ../../..
 
-if [ -e debian/usr/share/doc/hfstpy-dev/changelog.Debian ]; then
-    gzip --force --best debian/usr/share/doc/hfstpy-dev/changelog.Debian
-    chmod 644 debian/usr/share/doc/hfstpy-dev/changelog.Debian.gz
+if [ -e debian/usr/share/doc/hfstpy2-dev/changelog.Debian ]; then
+    gzip --force --best debian/usr/share/doc/hfstpy2-dev/changelog.Debian
+    chmod 644 debian/usr/share/doc/hfstpy2-dev/changelog.Debian.gz
+fi
+if [ -e debian/usr/share/doc/hfstpy3-dev/changelog.Debian ]; then
+    gzip --force --best debian/usr/share/doc/hfstpy3-dev/changelog.Debian
+    chmod 644 debian/usr/share/doc/hfstpy3-dev/changelog.Debian.gz
 fi
 
 fakeroot dpkg-deb --build debian
@@ -145,29 +156,31 @@ if test -e debian.deb; then
     fi
 fi
 
-# unzip the changelog file, so that svn is not confused because it is missing
-gunzip debian/usr/share/doc/hfstpy-dev/changelog.Debian.gz
-
 # delete temporary files
 
 if [ "$PYTHON_VERSION" = "2" ]; then
     rm -r debian/usr/lib/python2.7/* &&
     rmdir debian/usr/lib/python2.7 &&
+    # unzip the changelog file, so that svn is not confused because it is missing
+    gunzip debian/usr/share/doc/hfstpy2-dev/changelog.Debian.gz
+    sed -i "s/hfstpy2/HFSTPY/" debian/usr/share/doc/hfstpy2-dev/changelog.Debian
     mv debian/usr/share/doc/hfstpy2-dev debian/usr/share/doc/HFSTPY-DEV
-    sed -i "s/hfstpy3/HFSTPY/" debian/usr/share/doc/HFSTPY-DEV/changelog.Debian
     sed -i "s/hfstpy2-dev/HFSTPY-DEV/" debian/DEBIAN/control
     sed -i "s/hfstpy2/HFSTPY/" debian/DEBIAN/control
-    sed -i "s/python (>= 2.6)/python (>= PYTHON_MIN_VERSION)/" debian/DEBIAN/control
+    sed -i "s/, python/, PYTHON_NAME/" debian/DEBIAN/control
+    sed -i "s/PYTHON_NAME (>= 2.6)/PYTHON_NAME (>= PYTHON_MIN_VERSION)/" debian/DEBIAN/control
 else
     rm -r debian/usr/lib/python3/* &&
     rmdir debian/usr/lib/python3 &&
+    # unzip the changelog file, so that svn is not confused because it is missing
+    gunzip debian/usr/share/doc/hfstpy3-dev/changelog.Debian.gz
+    sed -i "s/hfstpy3/HFSTPY/" debian/usr/share/doc/hfstpy3-dev/changelog.Debian
     mv debian/usr/share/doc/hfstpy3-dev debian/usr/share/doc/HFSTPY-DEV
-    sed -i "s/hfstpy2/HFSTPY/" debian/usr/share/doc/HFSTPY-DEV/changelog.Debian
-    sed -i "s/hfstpy2-dev/HFSTPY-DEV/" debian/DEBIAN/control
-    sed -i "s/hfstpy2/HFSTPY/" debian/DEBIAN/control
-    sed -i "s/python (>= 3.2)/python (>= PYTHON_MIN_VERSION)/" debian/DEBIAN/control
+    sed -i "s/hfstpy3-dev/HFSTPY-DEV/" debian/DEBIAN/control
+    sed -i "s/hfstpy3/HFSTPY/" debian/DEBIAN/control
+    sed -i "s/, python3/, PYTHON_NAME/" debian/DEBIAN/control
+    sed -i "s/PYTHON_NAME (>= 3.2)/PYTHON_NAME (>= PYTHON_MIN_VERSION)/" debian/DEBIAN/control
 fi
-
 
 # To install the package, execute
 # sudo dpkg -i debian.deb
